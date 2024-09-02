6c9880b8b7ba5c2dd1924d0934fa6d7d0598d6b5
Benjamin Caldwell <8838361+bcaldwell@users.noreply.github.com>
2024-09-02 16:51:31 -0700

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-docker build --platform linux/arm/v8  -t benjamincaldwell/bento:latest-arm .
-docker build --platform linux/amd64  -t benjamincaldwell/bento:latest-amd .
-docker push benjamincaldwell/bento:latest-amd
-docker push benjamincaldwell/bento:latest-arm
+docker build --platform linux/arm/v8  -t ${DOCKER_IMAGE}:latest-arm .
+docker build --platform linux/amd64  -t ${DOCKER_IMAGE}:latest-amd .
+docker push ${DOCKER_IMAGE}:latest-amd
+docker push ${DOCKER_IMAGE}:latest-arm
 docker manifest create \
-benjamincaldwell/bento:$1 \
---amend benjamincaldwell/bento:latest-amd \
---amend benjamincaldwell/bento:latest-arm
-docker manifest push benjamincaldwell/bento:$1
+${DOCKER_IMAGE}:$1 \
+--amend ${DOCKER_IMAGE}:latest-amd \
+--amend ${DOCKER_IMAGE}:latest-arm
+docker manifest push ${DOCKER_IMAGE}:$1
 
+curl -H "Authorization: Bearer $(gh auth token)" "https://api.github.com/repos/${K8S_CONFIG_REPO}/actions/workflows/update-image.yaml/dispatches" -d '{"ref": "main", "inputs": {"repository": "'"${DOCKER_IMAGE}"'", "tag": "'"${1}"'"}}'
